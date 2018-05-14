1e7e2911e1a68b0440ff7066c273d1c19dcd69c6
electronicsmf <electronicsmf@aol.com>
2018-05-14 13:17:22 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
+
+import { CribsService } from './services/cribs.service';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent,
+    CribCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [CribsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/crib-listing/crib-listing.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Component({
+  selector: 'app-crib-listing',
+  templateUrl: './crib-listing.component.html',
+  styleUrls: ['./crib-listing.component.css']
+})
+export class CribListingComponent implements OnInit {
+
+  cribs: Array<any>;
+  error: string;
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.http.get('data/cribs.json')
+    .map(res => res.json())
+    .subscribe(
+      data => this.cribs = data,
+      error => this.error = error.statusText
+    );
+  }
+
+}
